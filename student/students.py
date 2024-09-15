e10b0ea2a56c3ecc07289c70530e473026f747ca
enas-12 <enas.magdy212>
2024-09-15 18:23:50 +0300

    Add First Student Class & Repo init

--- student/students.py
+++ student/students.py
@@ -2,15 +2,17 @@ import shortuuid
 import random
 
 class Student:
-    def __init__(self,Name,Gender,Grade,ID,Age,min_length, max_length):
+    def __init__(self, Name, Gender, Grade, ID, Age, min_length, max_length):
         self.name = Name
         self.age = Age
         uuid_length = random.randint(min_length, max_length)
         self.ID = str(shortuuid.ShortUUID().random(length=uuid_length))
         self.Gender = Gender
-        self.Grade = Grade
-        
+        self.Grade = Grade  # Assuming Grade is a list of numbers representing grades
 
     def display(self):
-            print(f"The details of Sudent1 are ( {self.name} , {self.age}, {self.ID},{self.Grade} and {self.Gender})")
-    
+        print(f"The details of Student1 are ({self.name}, {self.age}, {self.ID}, {self.Grade}, and {self.Gender})")
+       
+
+Student1 = Student("Enas", "Female", "14th", 500, 16, min_length=6, max_length=10)
+Student1.display()
