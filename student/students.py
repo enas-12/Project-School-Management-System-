12ab168a962291b1935c495d9d075cc231c4b8dc
momen124 <momenhossny2004@gmail.com>
2024-10-07 18:32:12 +0300

    refactor students

--- student/students.py
+++ student/students.py
@@ -1,25 +1,33 @@
+from sqlalchemy import Column, Integer, String, Float
+from database_setup import Base
 import shortuuid
 import random
 
-class Student:
-    students_data = []
-    def __init__(self, Name, Gender, Grade,Student_class, ID, Age, min_length=6, max_length=10):
-        self.name = Name
-        self.age = Age
-        self.Student_class = Student_class
-        uuid_length = random.randint(min_length, max_length)
-        self.ID = str(shortuuid.ShortUUID().random(length=uuid_length))
+class Student(Base):
+    __tablename__ = 'students'
+
+    ID = Column(String(10), primary_key=True)
+    Name = Column(String(50), nullable=False)
+    Gender = Column(String(1), nullable=False)
+    Age = Column(Integer, nullable=False)
+    Student_class = Column(Integer, nullable=False)
+    Grade = Column(String)  # Store grades as a comma-separated string
+
+    def __init__(self, Name, Gender, Grade, Student_class, Age, min_length=6, max_length=10):
+        self.ID = str(shortuuid.ShortUUID().random(length=random.randint(min_length, max_length)))
+        self.Name = Name
         self.Gender = Gender
-        self.Grade = Grade  # Assuming Grade is a list of numbers representing grades
+        self.Grade = ','.join(map(str, Grade))  # Store grades as a string
+        self.Student_class = Student_class
+        self.Age = Age
 
     def Calculate_Average(self):
-        if len(self.Grade) == 0:
+        grades_list = list(map(int, self.Grade.split(',')))
+        if len(grades_list) == 0:
             return 0
-        return sum(self.Grade) / len(self.Grade)
+        return sum(grades_list) / len(grades_list)
 
-    # Example grade list
     def display(self):
-        print(f"The details of Student1 are ({self.name},{self.age} years old ,in {self.Student_class}th , her ID is {self.ID} and {self.Gender})")
-        print(f"Average Grade: {self.Calculate_Average()}")
-
-
+        avg_grade = self.Calculate_Average()
+        print(f"Student: {self.Name}, ID: {self.ID}, Age: {self.Age}, Class: {self.Student_class}")
+        print(f"Gender: {self.Gender}, Average Grade: {avg_grade}")
